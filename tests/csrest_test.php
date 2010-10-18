03bb74c1a95921269eaf598b68a125c342320f43
Toby Brain <tobyb@freshview.com>
2010-10-19 10:21:49 +1100

    Added tests

--- tests/csrest_test.php
+++ tests/csrest_test.php
@@ -59,24 +59,57 @@ class CS_REST_TestBase extends UnitTestCase {
 		);
 	}
 	
-	function general_get_test($wrapper_function, $call_options, $from_transport, 
-	    $from_deserialisation, $response_code = 200) {
-	    	
+	function setup_transport_and_serialisation($make_call_result, $call_options, 
+	    $deserialise_result, $deserialise_input, 
+	    $serialise_result = NULL, $serialise_input = NULL) {
+	    		    
+		$this->mock_transport->setReturnValue('make_call', $make_call_result);
+		$this->mock_transport->expectOnce('make_call', array(new IdenticalExpectation($call_options)));
+		
+		$this->mock_serialiser->setReturnValue('deserialise', $deserialise_result);
+		$this->mock_serialiser->expectOnce('deserialise', array(new IdenticalExpectation($deserialise_input)));
+
+		if(!is_null($serialise_result)) {		
+			$this->mock_serialiser->setReturnValue('serialise', $serialise_result);
+			$this->mock_serialiser->expectOnce('serialise', array(new IdenticalExpectation($serialise_input)));			
+		}
+	}
+	
+	function general_test($wrapper_function, $call_options, $from_transport, 
+	    $from_deserialisation, $response_code = 200) {	
+	    		    	
 	    $expected_result = array (
 	        'code' => $response_code, 
 	        'response' => $from_transport
 	    );
 	    
-		$this->mock_transport->setReturnValue('make_call', $expected_result);
-		$this->mock_transport->expectOnce('make_call', array(new IdenticalExpectation($call_options)));
-		
-		$this->mock_serialiser->setReturnValue('deserialise', $from_deserialisation);
-		$this->mock_serialiser->expectOnce('deserialise', array(new IdenticalExpectation($from_transport)));
-	    	
-	    $result = $this->wrapper->$wrapper_function();
+	    $this->setup_transport_and_serialisation($expected_result, $call_options, 
+	        $from_deserialisation, $from_transport, NULL, NULL, $response_code);
+	        	    
+	    $result = $this->wrapper->$wrapper_function(); 	
 	    
 	    $expected_result['response'] = $from_deserialisation;
 	    $this->assertIdentical($expected_result, $result);		
+	}
+	
+	function general_test_with_argument($wrapper_function, $function_argument, $call_options, 
+	    $from_transport, $from_deserialisation, 
+	    $from_serialisation = 'serialised', $response_code = 200) {
+	    		    	
+	    $expected_result = array (
+	        'code' => $response_code, 
+	        'response' => $from_transport
+	    );
+	    
+	    $call_options['data'] = $from_serialisation;
+	    $this->setup_transport_and_serialisation($expected_result, $call_options, 
+	        $from_deserialisation, $from_transport, 
+	        $from_serialisation, $function_argument, $response_code);
+	    	
+		$result = $this->wrapper->$wrapper_function($function_argument); 	
+	    
+	    $expected_result['response'] = $from_deserialisation;
+	    $this->assertIdentical($expected_result, $result);		    	
 	}
 }
 
@@ -85,26 +118,26 @@ class CS_REST_TestWrapperBase extends CS_REST_TestBase {
 		$raw_result = 'some timezones';
 		$deserialised = array('timezone1', 'timezone2');
 		$call_options = $this->get_call_options(
-		    $this->base_route.'timezones.'.$this->mock_serialiser->get_format());
+		    $this->base_route.'timezones.'.$this->format);
 		
-		$this->general_get_test('get_timezones', $call_options, $raw_result, $deserialised);
+		$this->general_test('get_timezones', $call_options, $raw_result, $deserialised);
 	}
 	
 	function testget_systemdate() {
 		$raw_result = 'system date';
 		$call_options = $this->get_call_options(
-		    $this->base_route.'systemdate.'.$this->mock_serialiser->get_format());
+		    $this->base_route.'systemdate.'.$this->format);
 		
-		$this->general_get_test('get_systemdate', $call_options, $raw_result, $raw_result);
+		$this->general_test('get_systemdate', $call_options, $raw_result, $raw_result);
 	}
 	
 	function testget_countries() {
 		$raw_result = 'some countries';
 		$deserialised = array('Australia', 'Suid Afrika');
 		$call_options = $this->get_call_options(
-		    $this->base_route.'countries.'.$this->mock_serialiser->get_format());
+		    $this->base_route.'countries.'.$this->format);
 		
-		$this->general_get_test('get_countries', $call_options, $raw_result, $deserialised);
+		$this->general_test('get_countries', $call_options, $raw_result, $deserialised);
 	}
 	
 	function testget_apikey() {
@@ -114,19 +147,16 @@ class CS_REST_TestWrapperBase extends CS_REST_TestBase {
 		$site_url = 'unit.test.createsend.com';
 		
 		$call_options = $this->get_call_options(
-		    $this->base_route.'apikey.'.$this->mock_serialiser->get_format().'?siteurl='.$site_url);
+		    $this->base_route.'apikey.'.$this->format.'?siteurl='.$site_url);
 		$call_options['credentials'] = $username.':'.$password;
 	    	
 	    $expected_result = array (
 	        'code' => 200, 
 	        'response' => $raw_result
 	    );	
-		    		
-		$this->mock_transport->setReturnValue('make_call', $expected_result);
-		$this->mock_transport->expectOnce('make_call', array(new IdenticalExpectation($call_options)));
-		
-		$this->mock_serialiser->setReturnValue('deserialise', $raw_result);
-		$this->mock_serialiser->expectOnce('deserialise', array(new IdenticalExpectation($raw_result)));
+	    
+	    $this->setup_transport_and_serialisation($expected_result, $call_options, 
+	        $raw_result, $raw_result);
 		
 		$result = $this->wrapper->get_apikey($username, $password, $site_url);
 		$this->assertIdentical($expected_result, $result);		
@@ -136,8 +166,8 @@ class CS_REST_TestWrapperBase extends CS_REST_TestBase {
 		$raw_result = 'some clients';
 		$deserialised = array('Curran & Hughes', 'Repsol');
 		$call_options = $this->get_call_options(
-		    $this->base_route.'clients.'.$this->mock_serialiser->get_format());
+		    $this->base_route.'clients.'.$this->format);
 		
-		$this->general_get_test('get_clients', $call_options, $raw_result, $deserialised);	
+		$this->general_test('get_clients', $call_options, $raw_result, $deserialised);	
 	}
 }
