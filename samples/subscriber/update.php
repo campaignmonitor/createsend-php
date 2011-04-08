c0427ebdf30b668bfe75d00e73e6db0f78fd267b
Phil Gilmore <itgoeslikethis@fastmail.fm>
2011-04-08 14:31:37 +1000

    complete subscriber update via PUT

---
+++ samples/subscriber/update.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once '../../csrest_subscribers.php';
+
+$wrap = new CS_REST_Subscribers('Your list ID', 'Your API Key');
+$result = $wrap->update('Old Email Address', array(
+    'EmailAddress' => 'New Email Address',
+    'Name' => 'Subscriber name',
+    'CustomFields' => array(
+        array(
+            'Key' => 'Field name',
+            'Value' => 'Field Value'
+        )
+    ),
+    'Resubscribe' => true
+));
+
+echo "Result of PUT /api/v3/subscribers/{list id}.{format}?email={email}\n<br />";
+if($result->was_successful()) {
+    echo "Subscribed with code ".$result->http_status_code;
+} else {
+    echo 'Failed with code '.$result->http_status_code."\n<br /><pre>";
+    var_dump($result->response);
+    echo '</pre>';
+}
