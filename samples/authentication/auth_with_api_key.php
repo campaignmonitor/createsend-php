ee40ccb64b1d64b2a83aba71b7a225a79343f2dd
James Dennes <jdennes@gmail.com>
2013-02-17 14:33:19 +0100

    Added samples for authenticating using OAuth and an API key.

---
+++ samples/authentication/auth_with_api_key.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once '../../csrest_general.php';
+
+$auth = array('api_key' => 'your api key');
+$wrap = new CS_REST_General($auth);
+
+$result = $wrap->get_clients();
+
+
+echo "Result of /api/v3/clients\n<br />";
+if($result->was_successful()) {
+    echo "Got clients\n<br /><pre>";
+    var_dump($result->response);
+} else {
+    echo 'Failed with code '.$result->http_status_code."\n<br /><pre>";
+    var_dump($result->response);
+}
+echo '</pre>';
---
+++ samples/authentication/auth_with_oauth.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '../../csrest_general.php';
+
+$auth = array(
+    'access_token' => 'your access token',
+    'refresh_token' => 'your refresh token');
+$wrap = new CS_REST_General($auth);
+
+$result = $wrap->get_clients();
+
+
+echo "Result of /api/v3/clients\n<br />";
+if($result->was_successful()) {
+    echo "Got clients\n<br /><pre>";
+    var_dump($result->response);
+} else {
+    echo 'Failed with code '.$result->http_status_code."\n<br /><pre>";
+    var_dump($result->response);
+}
+echo '</pre>';
