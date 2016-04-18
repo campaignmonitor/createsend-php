645984f4e96672642891873f018742c8fbf2feb5
Mike Amundsen <mike@dtdpro.com>
2016-04-18 17:18:55 -0400

    Add Constructor for CS_REST_DoNothingSerialiser

--- class/serialisation.php
+++ class/serialisation.php
@@ -47,6 +47,7 @@ class CS_REST_BaseSerialiser {
 }
 
 class CS_REST_DoNothingSerialiser extends CS_REST_BaseSerialiser {
+    function __construct() {}
     function get_type() { return 'do_nothing'; }
     function serialise($data) { return $data; }
     function deserialise($text) {
