956e91c42883191b48a3168bb850973ebd66f36f
Cameron Newman <cameronn@campaignmonitor.com>
2017-03-31 07:43:21 +1100

    Fixed tests

--- tests/csrest_events_test.php
+++ tests/csrest_events_test.php
@@ -18,7 +18,7 @@ class CS_REST_OAuthTestEvents extends CS_REST_TestEvents {
 }
 
 abstract class CS_REST_TestEvents extends CS_REST_TestBase {
-    var $client_id = 'fake';
+    var $client_id = 'fakeclientid';
     var $events_base_route;
 
     function set_up_inner() {
@@ -28,33 +28,40 @@ abstract class CS_REST_TestEvents extends CS_REST_TestBase {
     }
    
     function testtrack() {
-
-        $raw_result = '{"EventID":"56a3b255-8bd5-491f-b241-fb6ce7e4ccbf"}';
+        $client_id = 'fakeclientid';
+        $raw_result = 'the new event id';
         $email = 'test@email.com';
-        $event_type = 'test';
-        $data = array();
-
-
-        $deserialised = array('EventID' => '56a3b255-8bd5-491f-b241-fb6ce7e4ccbf');
+        $name = 'Widget Man!';
+        $data = array('ExampleField'=> 'Me');
         $response_code = 202;
-        $expected_result = new CS_REST_Wrapper_Result($deserialised, $response_code);
+
+        $call_options = $this->get_call_options($this->base_route.'events/'.$client_id.'/track', 'POST');
+
+        $event_info = array (
+            'ContactID' => array(
+                'Email' => 'test@email.com'
+            ),
+            'Name' => 'Widget Man!',
+            'Data' => array(
+                'ExampleField'=> 'Me'
+            )
+        );
 
         $transport_result = array (
             'code' => $response_code, 
             'response' => $raw_result
         );
-
-        $input_payload = array('Data' => $data);
         
+        $expected_result = new CS_REST_Wrapper_Result($raw_result, $response_code);
 
-        $call_options = $this->get_call_options($this->events_base_route.'track', 'POST');
-        $call_options['data'] = $input_payload;
+        $call_options['data'] = 'event info was serialised to this';
         
-        $this->setup_transport_and_serialisation($transport_result, $call_options,  $deserialised, $input_payload);
+        $this->setup_transport_and_serialisation($transport_result, $call_options,
+            $raw_result, $raw_result, 'event info was serialised to this', $event_info, $response_code);
 
+        $result = $this->wrapper->track($email, $name, $data);
 
-    	$result = $this->wrapper->track($email, $event_type, $data);    	
+        $this->assertIdentical($expected_result, $result);
 
-    	$this->assertIdentical($expected_result, $result);      
     }
 }
