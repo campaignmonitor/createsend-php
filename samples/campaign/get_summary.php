9c792a09d623011e127fd7e640a315c6db4af320
oliver.toon <oliver.toon@campaignmonitor.com>
2021-11-22 16:47:15 +1100

    EL-282 Update relevant sample versions

--- samples/campaign/get_summary.php
+++ samples/campaign/get_summary.php
@@ -8,7 +8,7 @@ $auth = array(
 $wrap = new CS_REST_Campaigns('Campaign ID to get the summary of', $auth);
 $result = $wrap->get_summary();
 
-echo "Result of GET /api/v3.1/campaigns/{id}/summary\n<br />";
+echo "Result of GET /api/v3.3/campaigns/{id}/summary\n<br />";
 if($result->was_successful()) {
     echo "Got summary\n<br /><pre>";
     var_dump($result->response);
